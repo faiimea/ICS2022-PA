46d0c48f7a10a7fa49059054a38994a8bced7c6c
tracer-ics2022 <tracer@njuics.org>
2022-09-22 18:30:49 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:30:49 up  1:27,  4 users,  load average: 0.29, 0.32, 0.31

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -44,12 +44,14 @@ static char* rl_gets() {
 
 static int cmd_c(char *args) {
   cpu_exec(-1);
+	printf("%d\n", nemu_state.state);
   return 0;
 }
 
 
 static int cmd_q(char *args) {
   //cpu_exec(-1);
+	printf("%d\n", nemu_state.state); 
   return -1;
 }
 
