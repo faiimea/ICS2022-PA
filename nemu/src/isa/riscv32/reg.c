f082868126596410af4f0b1255c2f77e0bcd1c4a
tracer-ics2022 <tracer@njuics.org>
2022-09-22 20:05:40 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:05:40 up  3:02,  4 users,  load average: 0.22, 0.32, 0.28

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,7 +26,7 @@ const char *regs[] = {
 void isa_reg_display() {
 	int i;
 	for (i = 0; i < 32; i++){
-		printf("%s %#x\n", regs[i], cpu.pc+i);
+		printf("%s %#x %x\n", regs[i], cpu.pc+i, cpu.gpr[i]);
 	}
 }
 
