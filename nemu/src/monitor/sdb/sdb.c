ee917aab03f134fa2dc300a567ccdb2d68f94adf
tracer-ics2022 <tracer@njuics.org>
2022-09-23 17:57:44 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:57:44 up  3:16,  4 users,  load average: 0.13, 0.14, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -134,8 +134,8 @@ static int cmd_x(char *args) {
 	arg = strtok(NULL, " ");
 	sscanf(arg, "%x", &expr);
 	for (i = 0; i < (N-1)/4 + 1; i++) {
-		printf("%x ", expr+i);
-		word_t start = expr+4*i;
+		printf("%x ", expr+i*16);
+		word_t start = expr+16*i;
 		for (j = 0; j < 4; j++) {
 			printf("%#x ", paddr_read(start+j, 4));
 		}
