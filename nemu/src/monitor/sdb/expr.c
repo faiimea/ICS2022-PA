d5fc4a270549135bfb6c4dce926de4be315b0f54
tracer-ics2022 <tracer@njuics.org>
2022-09-27 14:40:55 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:40:55 up 24 min,  3 users,  load average: 0.69, 0.67, 0.56

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,6 +120,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
+				printf("type=%d\n",rules[i].token_type);
         switch (rules[i].token_type) {
 					case '+': case '-': case '*': case '/': case '(': case ')':
 						tokens[nr_token++].type = rules[i].token_type;break;
