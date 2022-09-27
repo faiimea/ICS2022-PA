07a1d271ee9ef918aceb164a9034c7164ca20400
tracer-ics2022 <tracer@njuics.org>
2022-09-27 18:25:39 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:25:39 up 7 min,  3 users,  load average: 0.08, 0.19, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -175,6 +175,7 @@ int eval(int p, int q) {
 	else {
 		int op;
 		op = find_op(p, q);
+		printf("op%d\n", op);
 		if (op == -1) assert(0);
 		int val1 = eval(p, op - 1);
 		int val2 = eval(op + 1, q);
