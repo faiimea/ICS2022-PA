8d368923ec3abbb958f89b4ebc87357ee4229bab
tracer-ics2022 <tracer@njuics.org>
2022-09-29 20:43:24 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:24 up  2:23,  5 users,  load average: 0.32, 0.29, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -170,7 +170,7 @@ word_t expr(char *e, bool *success) {
   }
 	
   /* TODO: Insert codes to evaluate the expression. */
-	printf("%u\n", eval(0, nr_token-1));
+	printf("%d\n", eval(0, nr_token-1));
   return 0;
 }
 
