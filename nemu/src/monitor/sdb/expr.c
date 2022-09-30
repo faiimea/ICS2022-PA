10b517e8253502f712fbd718358f59bf344cbba1
tracer-ics2022 <tracer@njuics.org>
2022-09-30 23:07:19 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:07:19 up  4:17,  4 users,  load average: 0.07, 0.23, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -226,7 +226,7 @@ word_t eval(int p, int q) {
 		word_t val1 = eval(p, op - 1);
 		word_t val2 = eval(op + 1, q);
 
-		//printf("val1=%x val2=%x\n", val1, val2);
+		printf("val1=%x val2=%x\n", val1, val2);
 		switch (tokens[op].type) {
 			case '+': return val1 + val2;
 			case '-': return val1 - val2;
