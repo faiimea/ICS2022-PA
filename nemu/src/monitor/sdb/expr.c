ccfd962beb8c3de159f082aac7863ee90b36dde2
tracer-ics2022 <tracer@njuics.org>
2022-10-01 09:57:54 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:57:54 up 19 min,  3 users,  load average: 0.37, 0.24, 0.15

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,7 +193,7 @@ word_t eval(int p, int q) {
 		switch (tokens[p].type) {
 			case NUM: return atoi(tokens[p].str);
 			case HEXNUM: sscanf(tokens[p].str, "%x", &res);return res;
-			case REGISTER: printf("reg=%s\n", tokens[p].str);return isa_reg_str2val(tokens[p].str, success);
+			case REGISTER: return isa_reg_str2val(tokens[p].str, success);
 		}
 		/* Single token.
 		 * For now this token should be a number.
