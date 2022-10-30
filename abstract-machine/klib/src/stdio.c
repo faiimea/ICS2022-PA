2584751763489255b8fd2c79aaa33ff9d462f751
tracer-ics2022 <tracer@njuics.org>
2022-10-30 16:35:45 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:35:45 up 57 min,  3 users,  load average: 0.17, 0.07, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,49 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char* get_int(va_list *ap) {
+	int d = va_arg(*ap, int);
+	char* str= '\0';
+	if (d == 0) {
+		*str = '0';
+		return str;
+	}
+	if (d < 0) {
+		*str = '-';
+		str++;
+		d *= -1;
+	}
+	char *t;
+	while (d) {
+		*t = (char)(d % 10 + '0');
+		t++;
+		d /= 10;
+	}
+	while (t) {
+		*str = *t;
+		str++;
+		t--;
+	}
+	return str;
+}
+
+static int make_out(char *out, const char *fmt, va_list ap) {
+	while (*fmt) {
+		if (*fmt == '%') {
+			fmt++;
+			switch (*fmt) {
+				case 'd': strcat(out, get_int(&ap));break;
+				case 's': strcat(out, va_arg(ap, char*));break;
+			}
+		}
+		else {
+			fmt++;
+		}
+	}
+	*out++ = '\0';
+	return 0;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +57,12 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	va_start(ap, fmt);
+	make_out(out, fmt, ap);
+	va_end(ap);
+	return 0;
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
