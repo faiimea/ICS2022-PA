484784a4d360b657bbc3327429e4a6084cb07af0
tracer-ics2022 <tracer@njuics.org>
2022-11-07 20:48:24 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:48:24 up  3:47,  4 users,  load average: 0.40, 0.33, 0.19

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+	uint64_t high = *(uint32_t*)(RTC_ADDR+4);
+	uint64_t low = *(uint32_t*)(RTC_ADDR);
+  uptime->us = (high << 32) + low;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
