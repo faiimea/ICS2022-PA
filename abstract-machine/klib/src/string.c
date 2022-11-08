7bd8c770a08733d22acff0fccf93cfc84746fe78
tracer-ics2022 <tracer@njuics.org>
2022-11-08 19:23:57 +0800

    >  compile NEMU 211300016 孙震 Linux szz-Lenovo-Legion-Y9000P2021H 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:23:57 up  3:52,  4 users,  load average: 0.54, 0.45, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -44,7 +44,7 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-	int flag = 1;
+	int flag = 0;
 	while(*s2) {
 		flag = *s1 - *s2;
 		if(flag) {
@@ -58,7 +58,7 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-	int flag = 1;
+	int flag = 0;
 	while(n-- && *s2) {
 		flag = *s1 - *s2;
 		if (flag) {
